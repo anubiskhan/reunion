e9af93074d8d17342a5ff506d6c89360603b9005
Kelly Schroeder <kellyrschroeder@gmail.com>
2018-02-21 10:53:14 -0700

    Changes activities and add_activities to accomodate hash for easier access when looking for total attendees, what attendees have paid on each activity, and total cost/debt

--- test/reunion_test.rb
+++ test/reunion_test.rb
@@ -14,9 +14,9 @@ class ReunionTest < Minitest::Test
   def test_that_reunion_has_activities
     reunion = Reunion.new('Degoba')
 
-    assert_instance_of Array, reunion.activities
-    assert_instance_of Activity, reunion.activities[0]
-    assert_equal 'reuniting', reunion.activities[0].name
+    assert_instance_of Hash, reunion.activities
+    assert_instance_of Activity, reunion.activities[:reuniting]
+    assert_equal 'reuniting', reunion.activities[:reuniting].name
   end
 
   def test_activities_can_be_added_to_reunion
@@ -26,10 +26,30 @@ class ReunionTest < Minitest::Test
     reunion.add_activity('kinda meeting dad')
     reunion.add_activity('swamp stuffs', 400)
 
-    assert_equal 'jedi mind tricks', reunion.activities[1].name
-    assert_equal 'kinda meeting dad', reunion.activities[2].name
-    assert_equal 'swamp stuffs', reunion.activities[3].name
-    assert_equal 400.0, reunion.activities[3].cost?
+    assert_equal 'jedi mind tricks', reunion.activities['jedi mind tricks'].name
+    assert_equal 'kinda meeting dad', reunion.activities['kinda meeting dad'].name
+    assert_equal 'swamp stuffs', reunion.activities['swamp stuffs'].name
+    assert_equal 400.0, reunion.activities['swamp stuffs'].cost?
     assert_equal 4, reunion.activities.length
   end
+
+  def test_can_determine_entire_reunion_cost
+    reunion = Reunion.new('Degoba')
+
+    reunion.add_activity('jedi mind tricks', 12)
+    reunion.add_activity('kinda meeting dad', 18.42)
+    reunion.add_activity('swamp stuffs', 400)
+
+    assert_equal 430.42, reunion.expenses?
+  end
+
+  # def test_can_determine_all_reunion_attendees
+  #   reunion = Reunion.new('Degoba')
+  #
+  #   reunion.add_activity('jedi mind tricks', 12)
+  #   reunion.add_activity('kinda meeting dad', 18.42)
+  #   reunion.add_activity('swamp stuffs', 400)
+  #
+  #
+  # end
 end
