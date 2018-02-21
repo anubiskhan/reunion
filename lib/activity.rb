3fb23f76881793253943f4b4d1524a4783cfecbf
Kelly Schroeder <kellyrschroeder@gmail.com>
2018-02-21 09:20:21 -0700

    Adds name and participant capability to activity

--- lib/activity.rb
+++ lib/activity.rb
@@ -1,2 +1,11 @@
 class Activity
+  attr_reader :name, :participants
+  def initialize(name)
+    @name = name
+    @participants = []
+  end
+
+  def add_participants(person)
+    @participants.push(person)
+  end
 end
--- test/activity_test.rb
+++ test/activity_test.rb
@@ -4,4 +4,22 @@ require 'minitest/pride'
 require './lib/activity'
 
 class ActivityTest < Minitest::Test
+  def test_activity_can_have_name
+    activity = Activity.new('coding')
+
+    assert_equal 'coding', activity.name
+  end
+
+  def test_activity_can_have_participants
+    activity = Activity.new('coding')
+
+    activity.add_participants('Doug')
+
+    assert activity.participants.include?('Doug')
+
+    activity.add_participants('Judy')
+    activity.add_participants('Jake')
+
+    assert_equal 3, activity.participants.length
+  end
 end
