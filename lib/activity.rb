01f60f3cc4ec550a9fa0a6acb032ccdf9c7ec744
Kelly Schroeder <kellyrschroeder@gmail.com>
2018-02-21 09:30:18 -0700

    Adds activity cost. Finishes Iteration 1

--- lib/activity.rb
+++ lib/activity.rb
@@ -1,11 +1,16 @@
 class Activity
   attr_reader :name, :participants
-  def initialize(name)
+  def initialize(name, cost = 0)
     @name = name
     @participants = []
+    @cost = cost.to_f
   end
 
   def add_participants(person)
     @participants.push(person)
+  end
+
+  def total_cost?
+    @cost * @participants.length
   end
 end
